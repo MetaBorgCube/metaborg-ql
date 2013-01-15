4b4be613b1aed3f11b933ed60c0314d19c6a1d1b
Vlad Vergu <v.vergu@gmail.com>
2013-01-15 13:58:37 +0100

    * Bare bones project for QLS * Some missing ignores added

---
+++ org.spoofax.lang.lwc.qls/editor/java/org/spoofax/lang/lwc/qls/QLSParseController.java
@@ -0,0 +1,4 @@
+package org.spoofax.lang.lwc.qls;
+
+public class QLSParseController extends QLSParseControllerGenerated 
+{ }
---
+++ org.spoofax.lang.lwc.qls/syntax/QLS.pp
@@ -0,0 +1,2 @@
+%% Pretty printing table (see also QLS.generated.pp)
+[]
