9d0111e18b9b9a7f7f290ea4fc59cafb6682a24a
Gabriel Konat <gabrielkonat@gmail.com>
2014-03-28 10:47:03 +0100

    Create a new QL project and convert syntax to new format.

---
+++ org.spoofax.lang.lwc.ql2/editor/java/org/spoofax/lang/lwc/ql2/QLParseController.java
@@ -0,0 +1,4 @@
+package org.spoofax.lang.lwc.ql2;
+
+public class QLParseController extends QLParseControllerGenerated 
+{ }
---
+++ org.spoofax.lang.lwc.ql2/editor/java/org/spoofax/lang/lwc/ql2/strategies/Main.java
@@ -0,0 +1,10 @@
+package org.spoofax.lang.lwc.ql2.strategies;
+
+import org.strategoxt.lang.Context;
+
+public class Main {
+	@SuppressWarnings("unused")
+	public static void init(Context context) {
+		// Called when the editor is being initialized
+	}
+}
