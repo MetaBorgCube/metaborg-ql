88df6dc4d70e0ef795c9c2e08ac02e048116a45c
Guido Wachsmuth <g.h.wachsmuth@tudelft.nl>
2014-03-28 23:36:39 +0100

    first shot on dependencies

---
+++ org.spoofax.lang.lwc.ql2/trans/ql/dependencies.ts
@@ -0,0 +1,35 @@
+module ql/dependencies
+
+imports
+
+  include/QL
+
+relations
+
+  define transitive <depends-on:
+        
+ type rules
+ 
+  Ref(qid) has dependency qid
+  
+  Not(e) has dependency dep
+    where e has dependency dep
+   
+  And(e1, e2) + Or(e1, e2) +
+  Lt(e1, e2) + 
+  Plus(e1, e2) + Minus(e1, e2) + Mul(e1, e2) + Div(e1, e2) 
+    has dependency dep1 or dep2
+    where e1 has dependency dep1
+      and e2 has dependency dep2
+
+  Computed(qid, _, TExpr(_, e)):-
+    where e has dependency dep
+      and store qid <depends-on: dep
+  
+  Conditional(e, [Question(qid, _, _)]):-
+    where e has dependency dep
+      and store qid <depends-on: dep
+  
+  Conditional(e, [Computed(qid, _, _)]):-
+    where e has dependency dep
+      and store qid <depends-on: dep
