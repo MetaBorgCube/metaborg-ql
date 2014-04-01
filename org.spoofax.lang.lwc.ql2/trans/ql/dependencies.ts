e8eee90b7eefc4fe02ff9afe9a50fc9f6ebcbcf0
Guido Wachsmuth <g.h.wachsmuth@tudelft.nl>
2014-04-01 12:49:58 +0200

    fixed dependency combination

--- org.spoofax.lang.lwc.ql2/trans/ql/dependencies.ts
+++ org.spoofax.lang.lwc.ql2/trans/ql/dependencies.ts
@@ -20,7 +20,7 @@ type rules
   And(e1, e2) + Or(e1, e2) +
   Lt(e1, e2) + 
   Plus(e1, e2) + Minus(e1, e2) + Mul(e1, e2) + Div(e1, e2) 
-    has dependency dep1 or dep2
+    has dependency dep1 union dep2
     where e1 has dependency dep1
       and e2 has dependency dep2
 
