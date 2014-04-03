95ca5c933f3f8975dac64219e23316a3ee28a2c2
Gabriel Konat <gabrielkonat@gmail.com>
2014-04-03 17:53:03 +0200

    Disable on-save handler and properties view. Show some debug info.

--- org.spoofax.lang.lwc.ql2/editor/java/org/spoofax/lang/lwc/ql2/strategies/Main.java
+++ org.spoofax.lang.lwc.ql2/editor/java/org/spoofax/lang/lwc/ql2/strategies/Main.java
@@ -3,7 +3,6 @@ package org.spoofax.lang.lwc.ql2.strategies;
 import org.strategoxt.lang.Context;
 
 public class Main {
-	@SuppressWarnings("unused")
 	public static void init(Context context) {
 		// Called when the editor is being initialized
 	}
